791154900a348f06f1fb387ffaa353b32dea7e36
戴勋发 <252544724@qq.com>
2017-07-19 16:38:52 +0800

    登录验证码改造成功

--- js/login.js
+++ js/login.js
@@ -1,10 +1,32 @@
 $(function(){
+	
+	//调用验证码插件
+	var verifyCode = new GVerify("v_container");
+	
+	
+	
 	/*检查账号和密码是否可以登录*/
 	$('#subit').click(function(){
-		if($('#username').val() == 'admin' && $('#password').val() == 'admin'){
+		var res = verifyCode.validate($("#code_input").val());
+		if(res ==true && $('#username').val() == 'admin' && $('#password').val() == 'admin'){
 			$(this).attr("href","control.html");
 		}else{
 			alert('请输入正确的账号和密码');
 		}
+	});
+	//当用户按enter键的时候，跳转至后台管理界面
+	$(document).keydown(function(event){
+		if(event.keyCode == 13){
+			var res = verifyCode.validate($("#code_input").val());
+			if(res ==true && $('#username').val() == 'admin' && $('#password').val() == 'admin'){	
+				window.location.href='control.html';
+			}else{
+				alert('请输入正确账号和密码');
+			}
+		}
+	});
+	$('.huantu').click(function(){
+		verifyCode.validate($("#code_input").val());
 	})
+	
 });
