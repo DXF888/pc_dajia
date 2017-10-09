3bff48124be41cef5c8c7b391ab75b4647002277
戴 <252544724@qq.com>
2017-10-09 18:05:11 +0800

    演员和司仪筛选完成

--- js/hotelList.js
+++ js/hotelList.js
@@ -16,78 +16,7 @@ $(function() {
 	if(bj==1){
 		getdata(newData);
 	}
-	//	console.log(newData.page);
-//	if(bg) {
-//		//		bj = false;
-//
-//		//console.log(window.sessionStorage.getItem('hotelList'));
-//		var data2 = JSON.parse(window.sessionStorage.getItem('hotelList'));
-//		var pages = JSON.parse(window.sessionStorage.getItem('pages'));
-//		var num = JSON.parse(window.sessionStorage.getItem('num'));
-//		if(data2.response.length >= 1) {
-//			var hotelHtml = template("template", data2);
-//			$('#hotel').html(hotelHtml);
-//				
-//			//点击a标签的时候跳转
-//			$('#hotel li').click(function() {
-//				var id = $(this).attr('id');
-//				window.location.href = "hotelDetails.html?id=" + id;
-//			});
-//
-//			$('#filtrate .area li').each(function(index, value) {
-//
-//			})
-//			$('#filtrate .price li').each(function(index, value) {
-//
-//			});
-//			$('#filtrate .star li').each(function(index, value) {
-//
-//			})
-//			//分页
-//			if((num / 16) <= 0) {
-//				$('.fy').css('display', 'none');
-//			} else {
-//				$('.fy').css('display', 'block');
-//			}
-//			$("#page").paging({
-//				pageNo: num,
-//				totalPage: pages,
-//				totalSize: 300,
-//				callback: function(num) {
-//					//				console.log(num);
-//					newData.page = num;
-//					$.ajax({
-//						type: "post",
-//						url: "http://120.55.70.81:8088/dajiaserver/Rest/web/get_hotel_list",
-//						data: newData,
-//						async: true,
-//						success: function(data) {
-//							var data1 = JSON.stringify(data);
-//							window.sessionStorage.setItem('hotelList', data1);
-//							window.sessionStorage.setItem('pages', parseInt(data.response[0].num / 16));
-//							window.sessionStorage.setItem('num', num);
-//
-//							//console.log(window.sessionStorage.getItem('hotelList'));
-//							var hotelHtml = template("template", data);
-//							$('#hotel').html(hotelHtml);
-//
-//							//点击a标签的时候跳转
-//							$('#hotel li').click(function() {
-//								var id = $(this).attr('id');
-//								window.location.href = "hotelDetails.html?id=" + id;
-//							});
-//						}
-//					});
-//				}
-//			});
-//		} else {
-//			getdata(newData);
-//		}
-//
-//	} else {
-//		getdata(newData);
-//	}
-
+	
 	$('#filtrate .area li').on('click', function() {
 		$(this).siblings().css('color', '#7B7B7B');
 		$(this).css('color', '#F05053');
@@ -169,7 +98,7 @@ $(function() {
 				window.sessionStorage.setItem('hotelList', data1);
 				console.log(data.response.length);
 				if(data.response.length > 0) {
-					window.sessionStorage.setItem('pages', parseInt(data.response[0].num / 16));
+					window.sessionStorage.setItem('pages', parseInt(data.response[0].num / 16)+1);
 					if(parseInt(data.response[0].num / 16) <= 0) {
 						$('.fy').css('display', 'none');
 					} else {
@@ -179,7 +108,7 @@ $(function() {
 					//分页
 					$("#page").paging({
 						pageNo: 1,
-						totalPage: parseInt(data.response[0].num / 16),
+						totalPage: parseInt(data.response[0].num / 16)+1,
 						totalSize: 300,
 						callback: function(num) {
 							//						console.log(num);
@@ -192,7 +121,7 @@ $(function() {
 								success: function(data) {
 									var data1 = JSON.stringify(data);
 									window.sessionStorage.setItem('hotelList', data1);
-									window.sessionStorage.setItem('pages', parseInt(data.response[0].num / 16));
+									window.sessionStorage.setItem('pages', parseInt(data.response[0].num / 16)+1);
 									window.sessionStorage.setItem('num', num);
 
 									var hotelHtml = template("template", data);
